59ac08816b4534027ac9a9b8601d8b351dedd36d
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-17 13:16:45 +0000

    added api key

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ window.addEventListener('load', function () {
 function fetchMovies () {
     var url = 'https://api.themoviedb.org/3/movie/';
     var movieID = '284054';
+    var apiKey = '8d0895ad52684bc5aaf2a952c644aeb5';
+    var movieURL = url + movieID + '?api_key=' + apiKey;
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById('demo').innerHTML = http.responseText;
         }
     };
-    http.open('GET', url + movieID, true);
+    http.open('GET', movieURL, true);
     http.send();
 }
