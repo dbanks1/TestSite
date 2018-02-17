75bebc658703e2b8f4ca42b52928956070f722a4
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-17 13:40:03 +0000

    most popular movies

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ window.addEventListener('load', function () {
 })
 
 function fetchMovies () {
-    var url = 'https://api.themoviedb.org/3/movie/';
-    var movieID = 'top_rated';
+    var url = 'https://api.themoviedb.org/3/movie/popular';
     var apiKey = '?api_key=8d0895ad52684bc5aaf2a952c644aeb5';
-    var movieURL = url + movieID + apiKey;
+    var movieURL = url + apiKey;
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
