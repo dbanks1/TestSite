87682069a299dc4822dc87c921ffb1541bbc9f12
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-17 14:03:26 +0000

    test

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ window.addEventListener('load', function () {
 })
 
 function fetchMovies () {
-    var url = 'https://api.themoviedb.org/3/movie/popular?page=1/';
+    var url = 'https://api.themoviedb.org/3/movie/popular';
     var apiKey = '?api_key=8d0895ad52684bc5aaf2a952c644aeb5';
     var movieURL = url + apiKey;
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById('demo').innerHTML = http.responseText;
+            document.getElementById('demo').innerHTML = http.responseText.slice(0, 10);
         }
     };
     http.open('GET', movieURL, true);
