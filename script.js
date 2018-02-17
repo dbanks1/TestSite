45593ea847d185eceeca2bb9276d4f074dd95b6e
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-17 13:12:18 +0000

    Quick HTTP request test

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function () {
+    fetchMovies;
+})
+
+function fetchMovies () {
+    var url = 'https://api.themoviedb.org/3/movie/';
+    var movieID = '284054';
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('demo').innerHTML = http.responseText;
+        }
+    };
+    http.open('GET', url + movieID, true);
+    http.send();
+}
