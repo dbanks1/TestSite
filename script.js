c1feddec654c43a0c7ecf8ddcf6e2b02f0ec4b0f
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-17 13:28:55 +0000

    fetch top rated movies

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ window.addEventListener('load', function () {
 
 function fetchMovies () {
     var url = 'https://api.themoviedb.org/3/movie/';
-    var movieID = '335984';
-    var apiKey = '8d0895ad52684bc5aaf2a952c644aeb5';
-    var movieURL = url + movieID + '?api_key=' + apiKey;
+    var movieID = 'top_rated';
+    var apiKey = '?api_key=8d0895ad52684bc5aaf2a952c644aeb5';
+    var movieURL = url + movieID + apiKey;
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
