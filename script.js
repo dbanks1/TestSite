6320ac6662f3080c048f5019c823bc92f38bb026
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-21 21:30:02 +0000

    should work

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function addToPopMovieArray (data) {
  */
 function displayPopMovies(popMovieArray) {
     var movieList = document.getElementById('movieList');
-    for (var i = 0; i < popMovieArray.length; i++) {
+    for (var i = 0; i <= popMovieArray.length; i++) {
         var newListItem = document.createElement('li');
         var newImg = document.createElement('img');
         newListItem.id = 'moviePoster' + i;
