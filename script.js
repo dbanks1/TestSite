46191dd0e1d0498235e51a07b06979c91740d50a
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-21 18:43:28 +0000

    getting movies from api

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function () {
     fetchPopularMovies();
 })
 
+var movieArray = [];
+
 function fetchPopularMovies () {
     var url = 'https://api.themoviedb.org/3/movie/popular';
     var apiKey = '?api_key=8d0895ad52684bc5aaf2a952c644aeb5';
@@ -11,11 +13,19 @@ function fetchPopularMovies () {
         if (this.readyState == 4 && this.status == 200) {
             var popMovies = JSON.parse(http.responseText);
             if (popMovies != null) {
-                document.getElementById('demo').innerHTML = popMovies;
+                addToMovieArray(data);
             }
             
         }
     };
     http.open('GET', movieURL, true);
     http.send();
+}
+
+function addToMovieArray (data) {
+    moviesToShow = data.results;
+    for (var i = 0; i < moviesToShow.length; i++) {
+        movieArray.push(moviesToShow[i].title);
+        console.log(movieArray[0]);
+    }
 }
