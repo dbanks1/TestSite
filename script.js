dbff62ba18e95074dcd71e44f3051ed95de6bad7
dbanks1 <s6199721@live.tees.ac.uk>
2018-02-21 18:32:09 +0000

    update

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ window.addEventListener('load', function () {
     fetchMovies();
 })
 
-function fetchMovies () {
+var movieArray = [];
+
+function fetchPopularMovies () {
     var url = 'https://api.themoviedb.org/3/movie/popular';
     var apiKey = '?api_key=8d0895ad52684bc5aaf2a952c644aeb5';
     var movieURL = url + apiKey;
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById('demo').innerHTML = http.responseText.title;
+            var popMovies = JSON.parse(http.responseText);
+            if (popMovies != null) {
+                addToMovieArray(popMovies);
+            }
+            
         }
     };
     http.open('GET', movieURL, true);
     http.send();
+}
+
+function addToMovieArray () {
+
 }
